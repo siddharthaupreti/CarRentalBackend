dbaead9d326dfabc72bc60b1e439dffe3517434c
Siddhartha Upreti <sid876upreti@gmail.com>
2023-05-01 16:51:29 +0545

    recent changes done

--- HajurKoCarRental/Models/APIModels/GetDamageLogByUserID/GetDamageLogByUserIDResponse.cs
+++ HajurKoCarRental/Models/APIModels/GetDamageLogByUserID/GetDamageLogByUserIDResponse.cs
@@ -5,10 +5,10 @@
         public string Status { get; set; }
         public string Message { get; set; }
         public Guid DamageLogID { get; set; }
-        public Guid CarID { get; set; }
+        public string CarName { get; set; }
         public decimal PaymentFees { get; set; }
         public string Payed { get; set; }
-        public Guid UserID { get; set; }
+        public string UserName { get; set; }
         
     }
 }
